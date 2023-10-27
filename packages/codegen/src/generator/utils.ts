e0410a4459a3f6fdbb3ca4719fa9adc7c899b007
shouse <aowiej23s@gmail.com>
2023-10-27 09:58:23 +0700

    tweak(codegen): using `handlebars` to render file content

--- packages/codegen/src/generator/utils.ts
+++ packages/codegen/src/generator/utils.ts
@@ -1,19 +1,16 @@
+import * as fs from 'fs';
+import handlebars from 'handlebars';
 import * as path from 'path';
-import * as process from 'process';
 import * as prettier from 'prettier';
+import * as process from 'process';
 
 export const commentBlock = (docs: string | string[]) => {
   docs = Array.isArray(docs) ? docs : [docs];
 
-  if (!docs || docs.length === 0) {
-    return '';
-  } else {
-    return `
-/**
-${docs.map((line) => `* ${line.replaceAll(/\s+/g, ' ').trim()}`).join('\n')}
-**/
-        `;
-  }
+  const docsTemplateFilePath = resolveFilePath('packages/codegen/src/templates/docs.hbs');
+  const template = compileTemplate(docsTemplateFilePath);
+
+  return template({ docs: docs.map((line) => line.replaceAll(/\s+/g, ' ').trim()) });
 };
 
 export const resolveFilePath = (relativePath: string | string[]) => {
@@ -23,6 +20,10 @@ export const resolveFilePath = (relativePath: string | string[]) => {
 };
 
 export const PRETTIER_FORMAT_OPTION = await prettier.resolveConfig(resolveFilePath('./.prettierrc.js'));
+
+export const compileTemplate = (templateFilePath: string) => {
+  return handlebars.compile(fs.readFileSync(templateFilePath, 'utf8'));
+};
 
 export const format = (tsInput: string) => {
   return prettier.format(tsInput, { parser: 'babel-ts', ...PRETTIER_FORMAT_OPTION });
