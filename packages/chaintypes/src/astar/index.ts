4b44e949c132f77801de54857b61893fe5481de2
Thang X. Vu <thang@coongcrafts.io>
2023-10-28 00:54:03 +0700

    feat(chaintypes): regenerate chain types

--- packages/chaintypes/src/astar/index.ts
+++ packages/chaintypes/src/astar/index.ts
@@ -1,9 +1,13 @@
 import { GenericSubstrateApi } from '@delightfuldot/types';
 import { ChainConsts } from './consts';
+import { ChainStorage } from './query';
+import { RpcCalls } from './rpc';
 
 export * from './types';
 export * from './consts';
 
 export interface AstarApi extends GenericSubstrateApi {
+  rpc: RpcCalls;
   consts: ChainConsts;
+  query: ChainStorage;
 }
--- packages/chaintypes/src/moonbeam/index.ts
+++ packages/chaintypes/src/moonbeam/index.ts
@@ -1,11 +1,13 @@
 import { GenericSubstrateApi } from '@delightfuldot/types';
 import { ChainConsts } from './consts';
 import { ChainStorage } from './query';
+import { RpcCalls } from './rpc';
 
 export * from './types';
 export * from './consts';
 
 export interface MoonbeamApi extends GenericSubstrateApi {
+  rpc: RpcCalls;
   consts: ChainConsts;
   query: ChainStorage;
 }
