019d390a296cbd57e2ee2791a911e6c1dc8130a0
shouse <aowiej23s@gmail.com>
2023-10-27 11:02:17 +0700

    refactor: optimize code

--- packages/codegen/src/generator/utils.ts
+++ packages/codegen/src/generator/utils.ts
@@ -24,6 +24,6 @@ export const resolveFilePath = (relativePath: string | string[]) => {
 
 export const PRETTIER_FORMAT_OPTION = await prettier.resolveConfig(resolveFilePath('./.prettierrc.js'));
 
-export const format = (tsInput: string) => {
+export const beautifySourceCode = (tsInput: string): Promise<string> => {
   return prettier.format(tsInput, { parser: 'babel-ts', ...PRETTIER_FORMAT_OPTION });
 };
