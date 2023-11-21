500df1175cc59118886da2ebd695a98eefd5312a
shouse <aowiej23s@gmail.com>
2023-11-21 21:10:40 +0700

    feat: errors generator

---
+++ packages/codegen/src/generator/ErrorsGen.ts
@@ -0,0 +1,45 @@
+import { ApiGen } from './ApiGen';
+import { stringLowerFirst } from '@polkadot/util';
+import { beautifySourceCode, commentBlock, compileTemplate } from './utils';
+
+export class ErrorsGen extends ApiGen {
+  generate() {
+    const { pallets } = this.metadata;
+
+    this.typesGen.clearCache();
+    this.typesGen.typeImports.addKnownType('GenericChainErrors', 'GenericModuleError');
+
+    let defTypeOut = '';
+    for (let pallet of pallets) {
+      const error = pallet.error;
+      if (!error) {
+        continue;
+      }
+
+      const errors = this.#getErrorVariants(error);
+
+      defTypeOut += `${stringLowerFirst(pallet.name)}: {
+        ${errors?.map(({ name, docs }) => `${commentBlock(docs)}${name}: GenericModuleError`).join(',\n')}
+      },`;
+    }
+
+    const importTypes = this.typesGen.typeImports.toImports();
+    const template = compileTemplate('errors.hbs');
+
+    return beautifySourceCode(template({ importTypes, defTypeOut }));
+  }
+
+  #getErrorVariants(errorId: number) {
+    const def = this.metadata.types[errorId];
+    if (!def) {
+      throw new Error(`Error def not found ${JSON.stringify(def)}`);
+    }
+
+    const { tag, value } = def.type;
+    if (tag !== 'Enum') {
+      throw new Error(`Not an Error type ${JSON.stringify(def)}`);
+    }
+
+    return value.members;
+  }
+}
