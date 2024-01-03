4c4faa5f422cb888c9ce26798efd3d56e04e70e2
shouse <aowiej23s@gmail.com>
2024-01-03 16:00:39 +0700

    chore(rpc): reformat docs

--- packages/types/src/babe.ts
+++ packages/types/src/babe.ts
@@ -1,6 +1,8 @@
 import { registry } from './registry';
 
-// Holds information about the `slot`'s that can be claimed by a given key.
+/*
+ * Holds information about the `slot`'s that can be claimed by a given key.
+ */
 export interface EpochAuthorship {
   // The array of primary slots that can be claimed
   primary: number[];
