0ea01e80f7eb6956a314d7caafa200e4feefc6f4
Thang X. Vu <thang@coongcrafts.io>
2024-01-12 11:29:00 +0700

    publish: prerelease 0.0.1-alpha.15
     - delightfuldot@0.0.1-alpha.15
     - @delightfuldot/chaintypes@0.0.1-alpha.15
     - @delightfuldot/codecs@0.0.1-alpha.15
     - @delightfuldot/codegen@0.0.1-alpha.15
     - @delightfuldot/shape@0.0.1-alpha.15
     - @delightfuldot/specs@0.0.1-alpha.15
     - @delightfuldot/types@0.0.1-alpha.15
     - @delightfuldot/utils@0.0.1-alpha.15

--- packages/specs/src/packageInfo.ts
+++ packages/specs/src/packageInfo.ts
@@ -1,3 +1,3 @@
 // THIS FILE IS AUTO-GENERATED, DO NOT EDIT!
 
-export const packageInfo = { name: '@delightfuldot/specs', version: '0.0.1-alpha.14' };
+export const packageInfo = { name: '@delightfuldot/specs', version: '0.0.1-alpha.15' };
