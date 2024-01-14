b2dd4ac32dfa476ac4bc8ce71bd5102fff98b67b
1cedrus <aowiej23s@gmail.com>
2024-01-14 12:34:27 +0700

    tweak(codecs): resolve TODOs

--- packages/codecs/src/known/common.ts
+++ packages/codecs/src/known/common.ts
@@ -42,4 +42,9 @@ export type Option<T> = T | undefined;
 
 export type ResultPayload<Ok, Err> = $.ResultPayload<Ok, Err>;
 
-// TODO add typeIn for other primitives codecs!
+export type u8 = number;
+export type u16 = number;
+export type u32 = number;
+export type u64 = bigint;
+export type u128 = bigint;
+export type u256 = bigint;
