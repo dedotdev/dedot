f518c0cf43bf99c26dcc6ba59c076c94dafb13fe
Thang X. Vu <thang@coongcrafts.io>
2024-01-31 23:17:44 +0700

    tweak: raw extrinsic codec #34

--- packages/codecs/src/known/common.ts
+++ packages/codecs/src/known/common.ts
@@ -19,7 +19,7 @@ export type StorageData = $.Output<typeof $StorageData>;
 
 registerLooseCodecType({ $StorageKey, $StorageData });
 
-export const $Extrinsic = $.PrefixedHex;
+export const $RawExtrinsic = $.RawHex;
 
 export const $Bytes = $.RawHex;
 export type BytesLike = $.Input<typeof $Bytes>;
