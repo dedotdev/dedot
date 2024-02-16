c9f3df6d0ba9c3edc7e1357b18cfff15d0faf421
1cedrus <aowiej23s@gmail.com>
2024-02-16 21:29:09 +0700

    test(zombienet-tests): verify apis if it available #36

--- zombienet_tests/src/scripts.ts
+++ zombienet_tests/src/scripts.ts
@@ -1,4 +1,5 @@
 import { DelightfulApi } from 'delightfuldot';
+import { $Metadata } from '@delightfuldot/codecs';
 
 export const run = async (nodeName: any, networkInfo: any) => {
   console.log('nodeName', nodeName);
@@ -7,7 +8,13 @@ export const run = async (nodeName: any, networkInfo: any) => {
   const { wsUri } = networkInfo.nodesByName[nodeName];
 
   const api = await DelightfulApi.create(wsUri);
-  const validator = await api.query.session.validators();
-  console.log('validator.length', validator.length);
-  return validator.length;
+
+  const _version = await api.call.core.version();
+  const _metadataVersions = await api.call.metadata.metadataVersions();
+  const encodedMetadata = await api.call.metadata.metadataAtVersion(15);
+
+  const metadata = $Metadata.tryDecode(encodedMetadata);
+  metadata.latest.apis.forEach(({ name, methods }) => {
+    methods.forEach((method) => api.call[name][method.name]);
+  });
 };
