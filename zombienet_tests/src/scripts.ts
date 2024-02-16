d5e53fc936a670c3ec21588d6c22e507edda89f9
1cedrus <aowiej23s@gmail.com>
2024-02-16 22:53:26 +0700

    test(zombienet_tests): add comments and log information #36

--- zombienet_tests/src/scripts.ts
+++ zombienet_tests/src/scripts.ts
@@ -2,18 +2,20 @@ import { DelightfulApi } from 'delightfuldot';
 import { $Metadata } from '@delightfuldot/codecs';
 
 export const run = async (nodeName: any, networkInfo: any) => {
-  console.log('nodeName', nodeName);
-  console.log('networkInfo', networkInfo);
-
   const { wsUri } = networkInfo.nodesByName[nodeName];
 
   const api = await DelightfulApi.create(wsUri);
 
-  const _version = await api.call.core.version();
-  const _metadataVersions = await api.call.metadata.metadataVersions();
-  const encodedMetadata = await api.call.metadata.metadataAtVersion(15);
+  const version = await api.call.core.version();
+  console.log('Version', version);
 
+  const metadataVersions = await api.call.metadata.metadataVersions();
+  console.log('Metadata versions support', metadataVersions);
+
+  const encodedMetadata = await api.call.metadata.metadataAtVersion(15);
   const metadata = $Metadata.tryDecode(encodedMetadata);
+
+  // Checking if all apis specs are defined
   metadata.latest.apis.forEach(({ name, methods }) => {
     methods.forEach((method) => api.call[name][method.name]);
   });
