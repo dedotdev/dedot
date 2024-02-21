c78f6778f36b72e4f6d02cfc63e8febfab18d1ff
Thang X. Vu <thang@coongcrafts.io>
2024-02-21 23:39:28 +0700

    tweak: calculate mortality #34

--- packages/api/src/extrinsic/extensions/known/CheckMortality.ts
+++ packages/api/src/extrinsic/extensions/known/CheckMortality.ts
@@ -4,10 +4,10 @@ import { assert, isZeroHash, min } from '@delightfuldot/utils';
 import { numberToHex, u8aToHex } from '@polkadot/util';
 import { SignerPayloadJSON } from '@polkadot/types/types';
 
-export const MAX_FINALITY_LAG = 5;
-export const FALLBACK_MAX_HASH_COUNT = 250;
-export const FALLBACK_PERIOD = 6 * 1000;
-export const MORTAL_PERIOD = 5 * 60 * 1000;
+export const MAX_FINALITY_LAG: number = 5;
+export const FALLBACK_MAX_HASH_COUNT: number = 250;
+export const FALLBACK_PERIOD: number = 6 * 1000;
+export const MORTAL_PERIOD: number = 5 * 60 * 1000;
 
 /**
  * @description Check for transaction mortality.
@@ -20,6 +20,7 @@ export class CheckMortality extends SignedExtension<EraLike, Hash> {
     this.additionalSigned = (await this.api.rpc.chain.getBlockHash(this.#signingHeader!.number))!;
   }
 
+  // Ref: https://github.com/polkadot-js/api/blob/3bdf49b0428a62f16b3222b9a31bfefa43c1ca55/packages/api-derive/src/tx/signingInfo.ts#L34-L64
   async #determineSigningHeader(): Promise<Header> {
     const [header, finalizedHash] = await Promise.all([
       this.api.rpc.chain.getHeader(),
@@ -40,21 +41,37 @@ export class CheckMortality extends SignedExtension<EraLike, Hash> {
       this.api.rpc.chain.getHeader(finalizedHash),
     ]);
 
-    if (!finalizedHeader || currentHeader!.number - finalizedHeader.number > MAX_FINALITY_LAG) {
-      return currentHeader as Header;
+    assert(currentHeader, 'Cannot determine current header');
+
+    if (!finalizedHeader || currentHeader.number - finalizedHeader.number > MAX_FINALITY_LAG) {
+      return currentHeader;
     }
 
     return finalizedHeader;
   }
 
+  // Ref: https://github.com/polkadot-js/api/blob/3bdf49b0428a62f16b3222b9a31bfefa43c1ca55/packages/api-derive/src/tx/signingInfo.ts#L83-L93
   #calculateMortalLength() {
     return min(
-      BigInt(this.api.consts.system.blockHashCount || FALLBACK_MAX_HASH_COUNT),
+      BigInt(this.#getConst<number>('system', 'blockHashCount') || FALLBACK_MAX_HASH_COUNT),
       BigInt(MORTAL_PERIOD) /
-        // BigInt(this.api.consts.babe.expectedBlockTime) ||
-        (BigInt(this.api.consts.timestamp.minimumPeriod) * 2n || BigInt(FALLBACK_PERIOD)) +
+        BigInt(
+          this.#getConst<bigint>('babe', 'expectedBlockTime') ||
+            BigInt(this.#getConst<bigint>('timestamp', 'minimumPeriod') || 0) * 2n ||
+            FALLBACK_PERIOD,
+        ) +
         BigInt(MAX_FINALITY_LAG),
     );
+  }
+
+  #getConst<T>(pallet: string, name: string): T | undefined {
+    try {
+      return this.api.consts[pallet][name];
+    } catch {
+      // ignore this on purpose
+    }
+
+    return undefined;
   }
 
   toPayload(): Partial<SignerPayloadJSON> {
