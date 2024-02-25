a81ab15b9e93024d519219073cfedddf06337525
1cedrus <aowiej23s@gmail.com>
2024-02-25 15:00:39 +0700

    chore: resolve merge

--- packages/codecs/src/known/nfts.ts
+++
@@ -1,9 +0,0 @@
-import * as $ from '@delightfuldot/shape';
-
-export const $NftCollectionId = $.u32;
-
-export type NftCollectionId = $.Input<typeof $NftCollectionId>;
-
-export const $NftItemId = $.u32;
-
-export type NftItemId = $.Input<typeof $NftItemId>;
