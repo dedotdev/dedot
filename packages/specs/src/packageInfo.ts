d0aaaf353a6596d5dcd9277b241799bd544a2bd2
Thang X. Vu <thang@coongcrafts.io>
2024-02-27 01:19:25 +0700

    publish: prerelease 0.0.1-alpha.23
     - dedot@0.0.1-alpha.23
     - @dedot/chaintypes@0.0.1-alpha.23
     - @dedot/codecs@0.0.1-alpha.23
     - @dedot/codegen@0.0.1-alpha.23
     - @dedot/shape@0.0.1-alpha.23
     - @dedot/specs@0.0.1-alpha.23
     - @dedot/types@0.0.1-alpha.23
     - @dedot/utils@0.0.1-alpha.23

--- packages/specs/src/packageInfo.ts
+++ packages/specs/src/packageInfo.ts
@@ -1,3 +1,3 @@
 // THIS FILE IS AUTO-GENERATED, DO NOT EDIT!
 
-export const packageInfo = { name: '@dedot/specs', version: '0.0.1-alpha.22' };
+export const packageInfo = { name: '@dedot/specs', version: '0.0.1-alpha.23' };
