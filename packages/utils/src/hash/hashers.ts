d49a99fbe2eae2fb86283ed578dc845bce863a10
Thang X. Vu <thang@coongcrafts.io>
2024-03-19 00:31:22 +0700

    minor refactoring

--- packages/utils/src/hash/hashers.ts
+++ packages/utils/src/hash/hashers.ts
@@ -2,12 +2,21 @@ import { blake2AsU8a } from './blake2.js';
 import { xxhashAsU8a } from './xxhash.js';
 import { concatU8a } from '../concat.js';
 
-export const HASHERS: Record<string, (data: Uint8Array) => Uint8Array> = {
-  blake2_128: (data: Uint8Array) => blake2AsU8a(data, 128),
-  blake2_256: (data: Uint8Array) => blake2AsU8a(data, 256),
-  blake2_128Concat: (data: Uint8Array) => concatU8a(blake2AsU8a(data, 128), data),
-  twox128: (data: Uint8Array) => xxhashAsU8a(data, 128),
-  twox256: (data: Uint8Array) => xxhashAsU8a(data, 256),
-  twox64Concat: (data: Uint8Array) => concatU8a(xxhashAsU8a(data, 64), data),
-  identity: (data: Uint8Array) => data,
+export type HashFn = (data: Uint8Array) => Uint8Array;
+export const blake2_128: HashFn = (data: Uint8Array) => blake2AsU8a(data, 128);
+export const blake2_256: HashFn = (data: Uint8Array) => blake2AsU8a(data, 256);
+export const blake2_128Concat: HashFn = (data: Uint8Array) => concatU8a(blake2AsU8a(data, 128), data);
+export const twox128: HashFn = (data: Uint8Array) => xxhashAsU8a(data, 128);
+export const twox256: HashFn = (data: Uint8Array) => xxhashAsU8a(data, 256);
+export const twox64Concat: HashFn = (data: Uint8Array) => concatU8a(xxhashAsU8a(data, 64), data);
+export const identity: HashFn = (data: Uint8Array) => data;
+
+export const HASHERS: Record<string, HashFn> = {
+  blake2_128,
+  blake2_256,
+  blake2_128Concat,
+  twox128,
+  twox256,
+  twox64Concat,
+  identity,
 };
--- packages/utils/src/to.ts
+++ packages/utils/src/to.ts
@@ -6,27 +6,29 @@ import { u8aToHex } from './u8a.js';
 import { hexToU8a } from './hex.js';
 
 export function toHex(input: string | number | Uint8Array | HexString): HexString {
-  if (isHex(input)) {
-    return input;
-  } else if (isString(input)) {
-    return stringToHex(input);
-  } else if (isU8a(input)) {
+  if (isU8a(input)) {
     return u8aToHex(input);
   } else if (isNumber(input)) {
     return numberToHex(input);
+  } else if (isHex(input)) {
+    return input;
+  } else if (isString(input)) {
+    return stringToHex(input);
   }
 
   throw new Error(`Invalid input type of: ${input}`);
 }
 
 export function toU8a(input: string | number | Uint8Array | HexString): Uint8Array {
-  if (isHex(input)) {
-    return hexToU8a(input);
-  } else if (isString(input)) {
-    return stringToU8a(input);
-  } else if (isU8a(input)) {
+  if (isU8a(input)) {
     return input;
   } else if (isNumber(input)) {
     return numberToU8a(input);
-  } else throw new Error(`Invalid input type of: ${input}`);
+  } else if (isHex(input)) {
+    return hexToU8a(input);
+  } else if (isString(input)) {
+    return stringToU8a(input);
+  }
+
+  throw new Error(`Invalid input type of: ${input}`);
 }
