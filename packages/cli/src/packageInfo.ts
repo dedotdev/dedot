1c4342b78a0673530dbbba7d05bcaf7e26167743
Thang X. Vu <thang@coongcrafts.io>
2024-03-20 17:50:08 +0700

    chore: remove packageInfo.ts

--- packages/cli/src/packageInfo.ts
+++
@@ -1,3 +0,0 @@
-// THIS FILE IS AUTO-GENERATED, DO NOT EDIT!
-
-export const packageInfo = { name: '@dedot/cli', version: '0.0.1-alpha.28' };
--- packages/types/src/packageInfo.ts
+++
@@ -1,3 +0,0 @@
-// THIS FILE IS AUTO-GENERATED, DO NOT EDIT!
-
-export const packageInfo = { name: '@dedot/types', version: '0.0.1-alpha.28' };
