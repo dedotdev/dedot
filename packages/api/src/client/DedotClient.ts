3d7c2ecc08d8417ea1341f4b94fffcabe1962633
Thang X. Vu <thang@coongcrafts.io>
2024-04-18 09:53:01 +0700

    logs

--- packages/api/src/client/DedotClient.ts
+++ packages/api/src/client/DedotClient.ts
@@ -70,7 +70,7 @@ export class DedotClient<
    */
   protected override async doInitialize() {
     const rpcMethods: string[] = (await this.rpc.rpc_methods()).methods;
-    console.log('rpcMethods', rpcMethods);
+    console.dir(rpcMethods, { depth: null });
 
     this._chainHead = new ChainHead(this, { rpcMethods });
     this._chainSpec = new ChainSpec(this, { rpcMethods });
