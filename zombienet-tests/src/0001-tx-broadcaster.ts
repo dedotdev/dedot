582425e46658935f10ac0c5ee130325a90752bb1
Thang X. Vu <thang@coongcrafts.io>
2024-04-22 14:34:55 +0700

    update logging & unsub

--- zombienet-tests/src/0001-tx-broadcaster.ts
+++ zombienet-tests/src/0001-tx-broadcaster.ts
@@ -25,20 +25,22 @@ export const run = async (nodeName: any, networkInfo: any): Promise<any> => {
   const broadcastUntilRemark = async (txBroadcaster: TxBroadcaster) => {
     const defer = deferred<void>();
     if (!(await txBroadcaster.supported())) {
-      console.log(`${txBroadcaster} broadcaster is not supported, skip it!`);
+      // @ts-ignore
+      console.log(`${txBroadcaster.prefix}-prefixed broadcaster is not supported, skip it!`);
       return defer.resolve();
     }
 
     const { rawTx, sender: senderAddress } = await prepareRemarkTx(api);
 
-    const unsub = await txBroadcaster.broadcastTx(rawTx);
+    const stopBroadcast = await txBroadcaster.broadcastTx(rawTx);
 
-    await api.query.system.events((events) => {
+    const unsub = await api.query.system.events((events) => {
       events.forEach(({ event }) => {
         if (api.events.system.Remarked.is(event)) {
           const { sender, hash } = event.palletEvent.data;
           if (sender.address() === senderAddress && api.registry.hashAsHex(stringToHex('Hello world')) === hash) {
             console.log('Remark event found, stop broadcasting now!');
+            stopBroadcast();
             unsub();
             defer.resolve();
           }
