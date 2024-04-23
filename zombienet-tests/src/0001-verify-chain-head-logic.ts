41333f72f01efd1b23ea29bd9c4fcfc88930345d
Thang X. Vu <thang@coongcrafts.io>
2024-04-23 16:16:10 +0700

    verify chainHead_body, chainHead_header, chainHead_call

--- zombienet-tests/src/0001-verify-chain-head-logic.ts
+++ zombienet-tests/src/0001-verify-chain-head-logic.ts
@@ -1,5 +1,5 @@
 import { assert, deferred } from '@dedot/utils';
-import { ChainHead, JsonRpcClient } from 'dedot';
+import { $Header, $RuntimeVersion, ChainHead, JsonRpcClient, PortableRegistry, $, $Metadata } from 'dedot';
 
 export const run = async (nodeName: any, networkInfo: any): Promise<any> => {
   const { wsUri: endpoint } = networkInfo.nodesByName[nodeName];
@@ -12,6 +12,30 @@ export const run = async (nodeName: any, networkInfo: any): Promise<any> => {
   assert(chainHead.finalizedHash, 'ChainHead.bestHash is not defined');
   assert(chainHead.runtimeVersion, 'ChainHead.runtimeVersion is not defined');
   assert(chainHead.bestRuntimeVersion, 'ChainHead.bestRuntimeVersion is not defined');
+
+  // verify chainHead_header
+  const rawHeader = await chainHead.header();
+  const header = $Header.tryDecode(rawHeader);
+  assert(header.number, 'best block height is not found');
+
+  // verify chainHead_call
+  const bestHash = chainHead.bestHash;
+  const bestRuntime = chainHead.bestRuntimeVersion;
+  const rawRuntime = await chainHead.call('Core_version', '0x', bestHash);
+  const fetchedRuntime = $RuntimeVersion.tryDecode(rawRuntime);
+  assert(bestRuntime.specVersion === fetchedRuntime.specVersion, 'Spec version mismatch');
+
+  // verify chainHead_body
+  const rawMetadata = await chainHead.call('Metadata_metadata_at_version', '0x0f000000', bestHash);
+  const metadata = $.Option($.lenPrefixed($Metadata)).tryDecode(rawMetadata)!;
+  const txs = await chainHead.body(bestHash);
+  const registry = new PortableRegistry(metadata.latest);
+
+  const extrinsics = txs.map((tx) => registry.$Extrinsic.tryDecode(tx));
+  console.log(extrinsics.length, 'extrinsics found');
+  extrinsics.forEach((extrinsic, idx) => {
+    assert(extrinsic.call, 'Extrinsic call is not defined at index ' + idx);
+  });
 
   return await Promise.all(
     ['newBlock', 'bestBlock', 'finalizedBlock'].map((event) => {
