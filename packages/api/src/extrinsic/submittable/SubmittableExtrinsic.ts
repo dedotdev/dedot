6f51697915bf4d7cf501bd6bb4f8596a0bdd54f5
Thang X. Vu <thang@coongcrafts.io>
2024-04-27 15:19:13 +0700

    move shared tx logic to basic submittable extrinsic

--- packages/api/src/extrinsic/submittable/SubmittableExtrinsic.ts
+++ packages/api/src/extrinsic/submittable/SubmittableExtrinsic.ts
@@ -11,6 +11,7 @@ import {
   Unsub,
 } from '@dedot/types';
 import { assert, HexString, isFunction, isHex, u8aToHex } from '@dedot/utils';
+import { BaseSubmittableExtrinsic } from 'dedot/extrinsic/submittable/BaseSubmittableExtrinsic';
 import type { FrameSystemEventRecord } from '../../chaintypes/index.js';
 import { StorageQueryExecutor } from '../../executor/index.js';
 import type { ISubstrateClient } from '../../types.js';
@@ -22,80 +23,7 @@ import { isKeyringPair, signRaw } from './utils.js';
  * @name SubmittableExtrinsic
  * @description A wrapper around an Extrinsic that exposes methods to sign, send, and other utility around Extrinsic.
  */
-export class SubmittableExtrinsic extends Extrinsic implements ISubmittableExtrinsicLegacy {
-  constructor(
-    public api: ISubstrateClient,
-    call: IRuntimeTxCall,
-  ) {
-    super(api.registry, call);
-  }
-
-  async sign(fromAccount: AddressOrPair, options?: Partial<SignerOptions>) {
-    const address = isKeyringPair(fromAccount) ? fromAccount.address : fromAccount.toString();
-    const extra = new ExtraSignedExtension(this.api, {
-      signerAddress: address,
-      payloadOptions: options,
-    });
-
-    await extra.init();
-
-    const { signer } = options || {};
-
-    let signature;
-    if (isKeyringPair(fromAccount)) {
-      signature = u8aToHex(
-        signRaw(fromAccount, extra.toRawPayload(this.callHex).data as HexString, { withType: true }),
-      );
-    } else if (signer?.signPayload) {
-      const result = await signer.signPayload(extra.toPayload(this.callHex));
-      signature = result.signature;
-    } else {
-      throw new Error('Signer not found. Cannot sign the extrinsic!');
-    }
-
-    const { signatureTypeId } = this.registry.metadata!.extrinsic;
-    const $Signature = this.registry.findCodec(signatureTypeId);
-
-    this.attachSignature({
-      address: address,
-      signature: $Signature.tryDecode(signature),
-      extra: extra.data,
-    });
-
-    return this;
-  }
-
-  signAndSend(account: AddressOrPair, options?: Partial<SignerOptions>): Promise<Hash>;
-
-  signAndSend(account: AddressOrPair, callback: Callback<ISubmittableResult>): Promise<Unsub>;
-
-  signAndSend(
-    account: AddressOrPair,
-    options: Partial<SignerOptions>,
-    callback?: Callback<ISubmittableResult>,
-  ): Promise<Unsub>;
-
-  async signAndSend(
-    fromAccount: AddressOrPair,
-    partialOptions?: Partial<SignerOptions> | Callback<ISubmittableResult>,
-    maybeCallback?: Callback<ISubmittableResult>,
-  ): Promise<Hash | Unsub> {
-    const [options, callback] = this.#normalizeOptions(partialOptions, maybeCallback);
-    await this.sign(fromAccount, options);
-    return this.send(callback as any);
-  }
-
-  #normalizeOptions(
-    partialOptions?: Partial<SignerOptions> | Callback<ISubmittableResult>,
-    callback?: Callback<ISubmittableResult>,
-  ): [Partial<SignerOptions>, Callback<ISubmittableResult> | undefined] {
-    if (isFunction(partialOptions)) {
-      return [{}, partialOptions];
-    } else {
-      return [Object.assign({}, partialOptions), callback];
-    }
-  }
-
+export class SubmittableExtrinsic extends BaseSubmittableExtrinsic implements ISubmittableExtrinsicLegacy {
   async dryRun(account: AddressOrPair, optionsOrHash?: Partial<SignerOptions> | BlockHash): Promise<DryRunResult> {
     const dryRunFn = this.api.rpc.system_dryRun;
 
@@ -120,7 +48,7 @@ export class SubmittableExtrinsic extends Extrinsic implements ISubmittableExtri
 
           const [signedBlock, blockEvents] = await Promise.all([
             this.api.rpc.chain_getBlock(blockHash),
-            this.#getSystemEventsAt(blockHash),
+            this._getSystemEventsAt(blockHash),
           ]);
 
           const txIndex = (signedBlock as SignedBlock).block.extrinsics.findIndex(
@@ -139,11 +67,5 @@ export class SubmittableExtrinsic extends Extrinsic implements ISubmittableExtri
     } else {
       return this.api.rpc.author_submitExtrinsic(this.toHex());
     }
-  }
-
-  async #getSystemEventsAt(hash: BlockHash): Promise<FrameSystemEventRecord[]> {
-    const executor = new StorageQueryExecutor(this.api, hash);
-    const systemEventsFn = executor.execute('System', 'Events') as AsyncMethod<FrameSystemEventRecord[]>;
-    return systemEventsFn();
   }
 }
