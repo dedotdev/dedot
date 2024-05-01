b7c4ba3fa14907bbc12fd20bb56eaabd5b9266ea
1cedrus <aowiej23s@gmail.com>
2024-05-01 21:04:42 +0700

    feat: add cli command to generate typink

---
+++ packages/cli/src/commands/typink.ts
@@ -0,0 +1,56 @@
+import { generateContractTypesFromMetadata } from '@dedot/codegen';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { CommandModule } from 'yargs';
+
+type Args = {
+  contract?: string;
+  output?: string;
+  metadata?: string;
+  dts?: boolean;
+};
+
+export const typink: CommandModule<Args, Args> = {
+  command: 'typink',
+  describe: 'Generate contract types & APIs for a Substrate-based blockchain',
+  handler: async (yargs) => {
+    const { contract, output = '', metadata = '', dts = true } = yargs;
+
+    const outDir = path.resolve(output);
+    const metadataFile = path.resolve(metadata);
+    const extension = dts ? 'd.ts' : 'ts';
+
+    const rawMetadata = fs.readFileSync(metadataFile, 'utf-8');
+
+    console.log(`- Generating contract types via metadata ${metadata}`);
+    await generateContractTypesFromMetadata(rawMetadata, contract, outDir, extension);
+
+    console.log(`- DONE! Output: ${outDir}`);
+  },
+
+  builder: (yargs) => {
+    return yargs
+      .option('metadata', {
+        type: 'string',
+        describe: 'Path to contract metadata file',
+        alias: 'm',
+        requiresArg: true,
+      })
+      .option('contract', {
+        type: 'string',
+        describe: 'Contract name',
+        alias: 'c',
+      })
+      .option('output', {
+        type: 'string',
+        describe: 'Output folder to put generated files',
+        alias: 'o',
+      })
+      .option('dts', {
+        type: 'boolean',
+        describe: 'Generate .d.ts files',
+        alias: 'd',
+        default: true,
+      });
+  },
+};
