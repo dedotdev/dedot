831193550cc7db0166c2fe68e41ee9975e26661e
1cedrus <aowiej23s@gmail.com>
2024-05-10 11:08:35 +0700

    refactor: optimize code

--- packages/utils/src/u8a.ts
+++ packages/utils/src/u8a.ts
@@ -27,25 +27,11 @@ export function u8aToString(input?: Uint8Array | null): string {
 
 /**
  * Compare two Uint8Arrays for equality
- * @Ref: https://github.com/polkadot-js/common/blob/master/packages/util/src/u8a/eq.ts
  */
 export function u8aEq(a: Uint8Array, b: Uint8Array) {
-  if (a.length === b.length) {
-    const dvA = new DataView(a.buffer, a.byteOffset);
-    const dvB = new DataView(b.buffer, b.byteOffset);
-    const mod = a.length % 4 | 0;
-    const length = (a.length - mod) | 0;
-    for (let i = 0; i < length; i += 4) {
-      if (dvA.getUint32(i) !== dvB.getUint32(i)) {
-        return false;
-      }
-    }
-    for (let i = length, count = a.length; i < count; i++) {
-      if (a[i] !== b[i]) {
-        return false;
-      }
-    }
-    return true;
+  if (a.length !== b.length) {
+    return false;
   }
-  return false;
+
+  return a.every((byte, index) => b[index] === byte);
 }
