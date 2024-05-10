c68a8aca3cf00fc8b871b5db69abaf81d96abcdd
Thang X. Vu <thang@coongcrafts.io>
2024-05-10 15:09:29 +0700

    tweak logs

--- zombienet-tests/src/0001-check-payment-info.ts
+++ zombienet-tests/src/0001-check-payment-info.ts
@@ -15,12 +15,12 @@ export const run = async (nodeName: any, networkInfo: any) => {
   const apiV1 = await Dedot.new(new WsProvider(wsUri));
   const paymentInfoV1 = await getPaymentInfo(apiV1);
 
-  console.log('Payment Info V1', paymentInfoV1);
-  assert(typeof paymentInfoV1.partialFee === 'bigint', '[V1] Partial fee should be of type bigint');
+  console.log('[API-V1] Payment Info', paymentInfoV1);
+  assert(typeof paymentInfoV1.partialFee === 'bigint', '[API-V1] Partial fee should be of type bigint');
 
   const apiV2 = await DedotClient.new(new WsProvider(wsUri));
   const paymentInfoV2 = await getPaymentInfo(apiV2);
 
-  console.log('Payment Info V2', paymentInfoV2);
-  assert(typeof paymentInfoV2.partialFee === 'bigint', '[V2] Partial fee should be of type bigint');
+  console.log('[API-V2] Payment Info', paymentInfoV2);
+  assert(typeof paymentInfoV2.partialFee === 'bigint', '[API-V2] Partial fee should be of type bigint');
 };
