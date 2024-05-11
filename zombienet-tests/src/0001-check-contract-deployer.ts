cc5d44f6bf99202e9b4c3a3c4033b59db02433b8
1cedrus <aowiej23s@gmail.com>
2024-05-12 01:13:20 +0700

    tests(e2e-tests): update tests

--- zombienet-tests/src/0001-check-contract-deployer.ts
+++ zombienet-tests/src/0001-check-contract-deployer.ts
@@ -1,6 +1,6 @@
 import Keyring from '@polkadot/keyring';
 import { cryptoWaitReady } from '@polkadot/util-crypto';
-import { Contract, ContractDeployer, parseRawMetadata } from '@dedot/contracts';
+import { ContractDeployer, parseRawMetadata } from '@dedot/contracts';
 import { assert, Dedot, WsProvider } from 'dedot';
 import * as flipperRaw from '../flipper.json';
 
@@ -14,44 +14,27 @@ export const run = async (nodeName: any, networkInfo: any) => {
   const api = await Dedot.new(new WsProvider(wsUri));
   const contractDeployer = new ContractDeployer(api, flipper, flipper.source.wasm!);
 
-  const { gasRequired } = await contractDeployer.query.new(flipper.source.hash, alicePair.address, {
-    caller: alicePair.address,
-    value: 0n,
-    salt: '0x',
+  const { gasRequired } = await contractDeployer.query.new(true, { caller: alicePair.address, value: 0n, salt: '0x' });
+  const constructorTx = contractDeployer.tx.new(true, { value: 0n, gasLimit: gasRequired, salt: '0x' });
+
+  let contractAddress: string | null = null;
+
+  return new Promise(async (resolve) => {
+    await constructorTx.signAndSend(alicePair, async ({ status, events }: any) => {
+      console.log('Transaction status', status.tag);
+
+      if (status.tag === 'InBlock') {
+        assert(
+          events.some(({ event }: any) => api.events.contracts.Instantiated.is(event)),
+          'Event Contracts.Instantiated should be available',
+        );
+
+        contractAddress = events
+          .find(({ event }: any) => api.events.contracts.Instantiated.is(event))
+          .event.data[1].raw.toString();
+
+        console.log('Contract address:', contractAddress);
+      }
+    });
   });
-
-  const constructorTx = contractDeployer.tx.new(flipper.source.hash, alicePair.address, {
-    value: 0n,
-    gasRequired,
-    salt: '0x',
-  });
-
-  let contractAddress: string = '';
-
-  await constructorTx.signAndSend(alicePair, async ({ status, events }: any) => {
-    console.log('Transaction status', status.tag);
-
-    if (status.tag === 'InBlock') {
-      assert(
-        events.some(({ event }: any) => api.events.contracts.Instantiated.is(event)),
-        'Event Contracts.Instantiated should be available',
-      );
-
-      contractAddress = events
-        .find(({ event }: any) => api.events.contracts.Instantiated.is(event))
-        .event.data[1].raw.toString();
-    }
-  });
-
-  console.assert(contractAddress, 'Contract address should be available');
-
-  const contract = new Contract(api, contractAddress, flipper);
-
-  const value = await contract.query.get();
-  console.log('Initial value:', value.toString());
-
-  await contract.tx.flip().signAndSend(alicePair);
-
-  const newValue = await contract.query.get();
-  console.log('New value:', newValue.toString());
 };
