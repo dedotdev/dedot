d9aff57dcbd8e15455da9627cbc43f07cb6c0afc
Thang X. Vu <thang@coongcrafts.io>
2024-05-15 15:45:03 +0700

    fix index gen file

--- packages/codegen/src/generator/IndexGen.ts
+++ packages/codegen/src/generator/IndexGen.ts
@@ -1,4 +1,5 @@
 import { stringPascalCase } from '@dedot/utils';
+import { TypeImports } from './TypeImports.js';
 import { beautifySourceCode, compileTemplate } from './utils.js';
 
 export class IndexGen {
@@ -7,8 +8,13 @@ export class IndexGen {
   async generate(useSubPaths: boolean = false) {
     const interfaceName = stringPascalCase(this.chain);
 
+    const typeImports = new TypeImports();
+    typeImports.addKnownType('GenericSubstrateApi', 'RpcLegacy', 'RpcV2', 'RpcVersion');
+
+    const importTypes = typeImports.toImports({ useSubPaths });
+
     const template = compileTemplate('index.hbs');
 
-    return beautifySourceCode(template({ interfaceName }));
+    return beautifySourceCode(template({ importTypes, interfaceName }));
   }
 }
