cb0ec5e83f8ccaf09b2eb21bbb2247aa88a32b91
Thang X. Vu <thang@coongcrafts.io>
2024-05-17 14:14:13 +0700

    fix missing tests

--- zombienet-tests/src/0001-verify-chain-head-logic.ts
+++ zombienet-tests/src/0001-verify-chain-head-logic.ts
@@ -1,7 +1,6 @@
-import { ChainHead, JsonRpcClient, QueryableStorage, PinnedBlock, WsProvider } from 'dedot';
+import { ChainHead, JsonRpcClient, $, QueryableStorage, PinnedBlock, WsProvider } from 'dedot';
 import { FrameSystemAccountInfo } from 'dedot/chaintypes';
 import { $Header, $RuntimeVersion, PortableRegistry, $Metadata, AccountId32 } from 'dedot/codecs';
-import * as $ from 'dedot/shape';
 import { assert, deferred, HexString } from 'dedot/utils';
 
 const ALICE = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
