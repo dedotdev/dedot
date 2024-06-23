9e56594700e0dac00f95fbd2c0c4a6848a7a216e
Thang X. Vu <thang@coongcrafts.io>
2024-06-23 14:53:14 +0700

    add StorageWeightReclaim & logs

---
+++ packages/api/src/extrinsic/extensions/known/StorageWeightReclaim.ts
@@ -0,0 +1,6 @@
+import { SignedExtension } from '../SignedExtension.js';
+
+/**
+ * @description Block resource (weight) limit check.
+ */
+export class StorageWeightReclaim extends SignedExtension {}
