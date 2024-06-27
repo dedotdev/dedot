0bcb5b681968e3c8abda070fe262b81dbcde5dac
1cedrus <aowiej23s@gmail.com>
2024-06-27 14:38:26 +0700

    chore: re-generate e2e-tests contract metadata

--- zombienet-tests/src/contracts/flipper/events.d.ts
+++ zombienet-tests/src/contracts/flipper/events.d.ts
@@ -4,5 +4,26 @@ import { GenericSubstrateApi } from '@dedot/types';
 import type { GenericContractEvent, GenericContractEvents } from 'dedot/contracts';
 
 export interface ContractEvents<ChainApi extends GenericSubstrateApi> extends GenericContractEvents<ChainApi> {
-  Flipped: GenericContractEvent<'Flipped', { old: boolean; new: boolean }>;
+  /**
+   * Emitted when the flip function is called.
+   *
+   * @signature_topic: 0x0a39b5ca0b8b3a5172476100ae7b9168b269cc91d5648efe180c75d935d3e886
+   **/
+  Flipped: GenericContractEvent<
+    'Flipped',
+    {
+      /**
+       * The previous state of the flip.
+       *
+       * @indexed: false
+       **/
+      old: boolean;
+      /**
+       * The new state of the flip.
+       *
+       * @indexed: false
+       **/
+      new: boolean;
+    }
+  >;
 }
