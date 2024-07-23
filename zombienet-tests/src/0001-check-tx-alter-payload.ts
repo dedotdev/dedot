b50b06bdf3c8a1953af83e43fe7f86b2e2091084
Thang X. Vu <thang@coongcrafts.io>
2024-07-23 10:51:19 +0700

    add e2e test

---
+++ zombienet-tests/src/0001-check-tx-alter-payload.ts
@@ -0,0 +1,61 @@
+import Keyring from '@polkadot/keyring';
+import { SignerPayloadJSON, SignerResult } from '@polkadot/types/types';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
+import { assert, u8aToHex } from '@dedot/utils';
+import { LegacyClient, WsProvider } from 'dedot';
+
+const BOB = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty';
+
+export const run = async (nodeName: any, networkInfo: any): Promise<void> => {
+  await cryptoWaitReady();
+  const keyring = new Keyring({ type: 'sr25519' });
+  const alice = keyring.addFromUri('//Alice');
+
+  const { wsUri } = networkInfo.nodesByName[nodeName];
+
+  const api = await LegacyClient.new(new WsProvider(wsUri));
+  const tip = 1_000n;
+
+  const alterSigner = {
+    signPayload: async (): Promise<SignerResult> => {
+      // Changing the payload to add some tip
+      // So we should expect the tip should be presence where the original tx does not have
+      // Proving that the original tx payload is already alter to add the tip
+      const transferTx = await api.tx.system.remarkWithEvent('Hello Dedot').sign(alice, { tip });
+
+      const { signatureTypeId } = api.registry.metadata!.extrinsic;
+      const $Signature = api.registry.findCodec(signatureTypeId);
+
+      return {
+        id: Date.now(),
+        signature: u8aToHex($Signature.tryEncode(transferTx.signature)),
+        signedTransaction: transferTx.toHex(),
+      };
+    },
+  };
+
+  return new Promise(async (resolve) => {
+    const unsub = await api.tx.system
+      .remarkWithEvent('Hello Dedot')
+      .signAndSend(alice.address, { signer: alterSigner }, async ({ status, events, dispatchInfo }) => {
+        console.log('Transaction status', status.type);
+
+        if (status.type === 'Finalized') {
+          const remarkEvent = events.map(({ event }) => event).find(api.events.system.Remarked.is);
+          const txFreePaidEvent = events
+            .map(({ event }) => event)
+            .find(api.events.transactionPayment.TransactionFeePaid.is);
+
+          assert(
+            remarkEvent && remarkEvent.pallet === 'System' && remarkEvent.palletEvent.name === 'Remarked',
+            'System.Remarked event should be emitted',
+          );
+
+          assert(txFreePaidEvent && txFreePaidEvent.palletEvent.data.tip === tip, 'Tip value is not correct');
+
+          await unsub();
+          resolve();
+        }
+      });
+  });
+};
