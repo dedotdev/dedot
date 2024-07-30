0ca24161c93c19c65df7335918a8ad928660f62d
Thang X. Vu <thang@coongcrafts.io>
2024-07-30 15:59:53 +0700

    add specs

--- packages/api/src/executor/utils.ts
+++ packages/api/src/executor/utils.ts
@@ -1,4 +1,26 @@
-import type { IEventRecord } from '@dedot/types';
+import type { IEventRecord, PalletEvent } from '@dedot/types';
+
+/**
+ * Checks if the provided object is a valid `PalletEvent`.
+ *
+ * @param event - The object to be checked.
+ * @returns `true` if the object is a valid `PalletEvent`, `false` otherwise.
+ *
+ * @remarks
+ * A `PalletEvent` is expected to have the following properties:
+ * - `pallet`: A string representing the name of the pallet associated with the event.
+ * - `palletEvent`: An object or string representing the details of the pallet event.
+ *
+ * This function validates the presence and type of these properties in the provided object.
+ */
+export const isPalletEvent = (event: any): event is PalletEvent => {
+  return (
+    'pallet' in event &&
+    typeof event['pallet'] === 'string' &&
+    'palletEvent' in event &&
+    (typeof event['palletEvent'] === 'object' || typeof event['palletEvent'] === 'string')
+  );
+};
 
 /**
  * Checks if the provided object is a valid `IEventRecord`.
@@ -19,7 +41,7 @@ export const isEventRecord = (event: any): event is IEventRecord => {
     'phase' in event &&
     typeof event['phase'] === 'object' &&
     'event' in event &&
-    typeof event['event'] === 'object' &&
+    isPalletEvent(event['event']) &&
     'topics' in event &&
     Array.isArray(event['topics'])
   );
