2688f1df805be4f420050ae9a902a2958714fd0e
1cedrus <aowiej23s@gmail.com>
2024-08-11 12:50:32 +0700

    remove unused code

--- packages/cli/src/commands/chaintypes/types.ts
+++ packages/cli/src/commands/chaintypes/types.ts
@@ -1,16 +1,5 @@
 import { Metadata, RuntimeVersion } from '@dedot/codecs';
 
-export type Args = {
-  wsUrl?: string;
-  output?: string;
-  chain?: string;
-  dts?: boolean;
-  subpath?: boolean;
-  runtimeFile?: string;
-  metadataFile?: string;
-};
-
-
 export interface ParsedResult extends DecodedResult {
   rpcMethods: string[];
 }
