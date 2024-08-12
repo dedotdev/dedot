301ac58da21b4dac00454c897f059dc59b2d6af6
1cedrus <aowiej23s@gmail.com>
2024-08-12 16:35:46 +0700

    add error throwing specs

--- packages/cli/src/__tests__/commands.spec.ts
+++ packages/cli/src/__tests__/commands.spec.ts
@@ -7,10 +7,30 @@ const RUNTIME_FILE = path.resolve(__dirname, 'runtime.wasm');
 
 describe('chaintypes', () => {
   it('should parse raw metadata file properly', async () => {
-    expect(await parseMetadataFromRaw(METADATA_FILE)).toBeTruthy();
+    const result = await parseMetadataFromRaw(METADATA_FILE);
+    expect(result).toBeTruthy();
+    expect(result.metadata.version).toEqual('V15');
+    expect(result.runtimeVersion).toBeTypeOf('object');
+    expect(result.runtimeApis).toBeTypeOf('object');
+    expect(result.rpcMethods).toEqual([]);
   });
+
+  it('should throw error for not-metadata file', async () => {
+    expect(parseMetadataFromRaw(RUNTIME_FILE)).rejects.toThrowError();
+  });
+
   it('should parse runtime wasm file properly', async () => {
-    expect(await parseMetadataFromWasm(RUNTIME_FILE)).toBeTruthy();
+    const result = await parseMetadataFromWasm(RUNTIME_FILE);
+    expect(result).toBeTruthy();
+
+    expect(result).toBeTruthy();
+    expect(result.metadata.version).toEqual('V15');
+    expect(result.runtimeVersion).toBeTypeOf('object');
+    expect(result.runtimeApis).toBeTypeOf('object');
+    expect(result.rpcMethods).toEqual([]);
+  });
+
+  it('should throw error for not-wasm file', async () => {
+    expect(parseMetadataFromWasm(METADATA_FILE)).rejects.toThrowError();
   });
 });
-
