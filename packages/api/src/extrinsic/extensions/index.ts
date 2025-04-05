58dfb1a4700c92192b89ae518ae349fd3131446d
Thang X. Vu <zthangxv@gmail.com>
2025-04-05 10:13:00 +0700

    fix imports

--- packages/api/src/extrinsic/extensions/index.ts
+++ packages/api/src/extrinsic/extensions/index.ts
@@ -2,4 +2,3 @@ export * from './SignedExtension.js';
 export * from './known/index.js';
 export * from './ExtraSignedExtension.js';
 export * from './NoopSignedExtension.js';
-export * from './utils.js';
