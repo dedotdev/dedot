c805ededeee7aa0c86cc137733f1f5475161d953
Thang X. Vu <zthangxv@gmail.com>
2025-04-27 20:14:29 +0700

    misc tweaks

--- packages/utils/src/queue/ThrottleQueue.ts
+++ packages/utils/src/queue/ThrottleQueue.ts
@@ -9,53 +9,50 @@ export type AsyncTask<T> = () => Promise<T>;
  * at which point they are queued and executed as running tasks complete.
  */
 export class ThrottleQueue {
-  private maxConcurrentTasks: number;
-  private running: number = 0;
-  private queue: { resolve: () => void; reject: (error: Error) => void }[] = [];
-  private _active: boolean = true;
+  #running: number = 0;
+  #queue: { resolve: () => void; reject: (error: Error) => void }[] = [];
+  #active: boolean = true;
 
   /**
    * Creates a new ThrottleQueue
-   * 
+   *
    * @param maxConcurrentTasks Maximum number of tasks to run concurrently
    */
-  constructor(maxConcurrentTasks: number) {
-    this.maxConcurrentTasks = maxConcurrentTasks;
-  }
+  constructor(private readonly maxConcurrentTasks: number) {}
 
   /**
    * Adds a task to the queue. The task will be executed immediately if
    * the concurrency limit has not been reached, otherwise it will be queued.
-   * 
+   *
    * @param task The async task to execute
    * @returns A promise that resolves with the task result
    * @throws If the queue has been cancelled
    */
   async add<T>(task: AsyncTask<T>): Promise<T> {
-    assert(this._active, 'Queue has been cancelled');
+    assert(this.#active, 'Queue has been cancelled');
 
     // If concurrency limit reached, wait
-    if (this.running >= this.maxConcurrentTasks) {
+    if (this.#running >= this.maxConcurrentTasks) {
       await new Promise<void>((resolve, reject) => {
-        this.queue.push({ 
-          resolve, 
-          reject: (error: Error) => reject(error)
+        this.#queue.push({
+          resolve,
+          reject: (error: Error) => reject(error),
         });
       });
-      
+
       // Check if queue was cancelled while waiting
-      assert(this._active, 'Queue has been cancelled');
+      assert(this.#active, 'Queue has been cancelled');
     }
 
-    this.running++;
+    this.#running++;
 
     try {
       return await task();
     } finally {
-      this.running--;
+      this.#running--;
       // Run next queued task if any
-      if (this.queue.length > 0) {
-        const next = this.queue.shift();
+      if (this.#queue.length > 0) {
+        const next = this.#queue.shift();
         if (next) next.resolve();
       }
     }
@@ -67,39 +64,39 @@ export class ThrottleQueue {
    */
   cancel(): void {
     // Set queue to inactive to reject any tasks that are waiting
-    this._active = false;
-    
+    this.#active = false;
+
     // Store the current queue and clear it
-    const currentQueue = [...this.queue];
-    this.queue = [];
-    
+    const currentQueue = [...this.#queue];
+    this.#queue = [];
+
     // Reject all pending promises
     currentQueue.forEach(({ reject }) => {
       reject(new Error('Queue has been cancelled'));
     });
-    
+
     // Reset the queue to active state for future tasks
-    this._active = true;
+    this.#active = true;
   }
 
   /**
    * Gets the number of tasks currently in the queue
    */
   get size(): number {
-    return this.queue.length;
+    return this.#queue.length;
   }
 
   /**
    * Gets the number of tasks currently running
    */
   get runningCount(): number {
-    return this.running;
+    return this.#running;
   }
 
   /**
    * Gets whether the queue is active
    */
   get active(): boolean {
-    return this._active;
+    return this.#active;
   }
 }
