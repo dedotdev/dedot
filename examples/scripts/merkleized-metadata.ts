bfbc9c8ddf910c0b42456130216f81b42c831e4a
Thang X. Vu <thang@dedot.dev>
2025-05-18 15:29:46 +0700

    add examples to connect to parachain, relaychain via smoldot

--- examples/scripts/merkleized-metadata.ts
+++ examples/scripts/merkleized-metadata.ts
@@ -12,35 +12,31 @@ import { u8aToHex } from 'dedot/utils';
  * tsx ./packages/merkleized-metadata/examples/calculate-hash.ts
  * ```
  */
-async function main() {
-  await cryptoWaitReady();
-  const keyring = new Keyring({ type: 'sr25519' });
-  const alice = keyring.addFromUri('//Alice');
+await cryptoWaitReady();
+const keyring = new Keyring({ type: 'sr25519' });
+const alice = keyring.addFromUri('//Alice');
 
-  // Create a dedot client
-  console.log('Connecting to Polkadot...');
-  const provider = new WsProvider('wss://rpc.polkadot.io');
-  const client = await DedotClient.create({ provider });
+// Create a dedot client
+console.log('Connecting to Polkadot...');
+const provider = new WsProvider('wss://rpc.polkadot.io');
+const client = await DedotClient.create({ provider });
 
-  // Get metadata from the client
-  const metadata = client.metadata;
-  console.log(`Connected to ${client.runtimeVersion.specName} v${client.runtimeVersion.specVersion}`);
+// Get metadata from the client
+const metadata = client.metadata;
+console.log(`Connected to ${client.runtimeVersion.specName} v${client.runtimeVersion.specVersion}`);
 
-  // Create a calculator instance
-  const merkleizer = new MerkleizedMetadata(metadata, {
-    decimals: 10,
-    tokenSymbol: 'DOT',
-  });
+// Create a calculator instance
+const merkleizer = new MerkleizedMetadata(metadata, {
+  decimals: 10,
+  tokenSymbol: 'DOT',
+});
 
-  const remarkTx = client.tx.system.remark('Hello World');
-  await remarkTx.sign(alice, { tip: 1000_000n });
+const remarkTx = client.tx.system.remark('Hello World');
+await remarkTx.sign(alice, { tip: 1000_000n });
 
-  console.log('TxHex', remarkTx.toHex());
-  console.log('Digest:', u8aToHex(merkleizer.digest()));
-  console.log('ProofForExtrinsic', u8aToHex(merkleizer.proofForExtrinsic(remarkTx.toHex())));
+console.log('TxHex', remarkTx.toHex());
+console.log('Digest:', u8aToHex(merkleizer.digest()));
+console.log('ProofForExtrinsic', u8aToHex(merkleizer.proofForExtrinsic(remarkTx.toHex())));
 
-  // Disconnect the client
-  await client.disconnect();
-}
-
-main().catch(console.error);
+// Disconnect the client
+await client.disconnect();
