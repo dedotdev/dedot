ee27a8374bf07526ac1ce5dde8b855705bce8b8c
Thang X. Vu <thang@dedot.dev>
2025-09-28 17:17:04 +0700

    throw

--- e2e/zombienet/src/0001-check-tx-transfer-balance-v2.ts
+++ e2e/zombienet/src/0001-check-tx-transfer-balance-v2.ts
@@ -6,46 +6,51 @@ import { assert, isHex, isNumber } from 'dedot/utils';
 const BOB = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty';
 
 export const run = async (nodeName: any, networkInfo: any): Promise<void> => {
-  await cryptoWaitReady();
-  const keyring = new Keyring({ type: 'sr25519' });
-  const alice = keyring.addFromUri('//Alice');
+  try {
+    await cryptoWaitReady();
+    const keyring = new Keyring({ type: 'sr25519' });
+    const alice = keyring.addFromUri('//Alice');
 
-  const { wsUri } = networkInfo.nodesByName[nodeName];
+    const { wsUri } = networkInfo.nodesByName[nodeName];
 
-  // TODO use RococoApi
-  const api = await DedotClient.new(new WsProvider(wsUri));
+    // TODO use RococoApi
+    const api = await DedotClient.new(new WsProvider(wsUri));
 
-  const prevBobBalance = (await api.query.system.account(BOB)).data.free;
-  console.log('BOB - old balance', prevBobBalance);
+    const prevBobBalance = (await api.query.system.account(BOB)).data.free;
+    console.log('BOB - old balance', prevBobBalance);
 
-  const TEN_UNIT = BigInt(10 * 1e12);
+    const TEN_UNIT = BigInt(10 * 1e12);
 
-  const transferTx = api.tx.balances.transferKeepAlive(BOB, TEN_UNIT);
+    const transferTx = api.tx.balances.transferKeepAlive(BOB, TEN_UNIT);
 
-  let blockIncluded: boolean = false;
-  await transferTx
-    .signAndSend(alice, async ({ status, txIndex }) => {
-      console.log('Transaction status', status.type);
-      if (status.type === 'BestChainBlockIncluded') {
-        assert(isHex(status.value.blockHash), 'Block hash should be hex');
-        assert(isNumber(status.value.blockNumber), 'Block number should be number');
-        assert(isNumber(status.value.txIndex), 'Tx index should be number');
-        assert(txIndex === status.value.txIndex, 'Mismatched tx index');
+    let blockIncluded: boolean = false;
+    await transferTx
+      .signAndSend(alice, async ({ status, txIndex }) => {
+        console.log('Transaction status', status.type);
+        if (status.type === 'BestChainBlockIncluded') {
+          assert(isHex(status.value.blockHash), 'Block hash should be hex');
+          assert(isNumber(status.value.blockNumber), 'Block number should be number');
+          assert(isNumber(status.value.txIndex), 'Tx index should be number');
+          assert(txIndex === status.value.txIndex, 'Mismatched tx index');
 
-        const newBobBalance = (await api.query.system.account(BOB)).data.free;
-        console.log('BOB - new balance', newBobBalance);
-        assert(prevBobBalance + TEN_UNIT === newBobBalance, 'Incorrect BOB balance');
-        blockIncluded = true;
-      }
+          const newBobBalance = (await api.query.system.account(BOB)).data.free;
+          console.log('BOB - new balance', newBobBalance);
+          assert(prevBobBalance + TEN_UNIT === newBobBalance, 'Incorrect BOB balance');
+          blockIncluded = true;
+        }
 
-      if (status.type === 'Finalized') {
-        assert(isHex(status.value.blockHash), 'Block hash should be hex');
-        assert(isNumber(status.value.blockNumber), 'Block number should be number');
-        assert(isNumber(status.value.txIndex), 'Tx index should be number');
-        assert(txIndex === status.value.txIndex, 'Mismatched tx index');
+        if (status.type === 'Finalized') {
+          assert(isHex(status.value.blockHash), 'Block hash should be hex');
+          assert(isNumber(status.value.blockNumber), 'Block number should be number');
+          assert(isNumber(status.value.txIndex), 'Tx index should be number');
+          assert(txIndex === status.value.txIndex, 'Mismatched tx index');
 
-        assert(blockIncluded, 'Finalized before block included');
-      }
-    })
-    .untilFinalized();
+          assert(blockIncluded, 'Finalized before block included');
+        }
+      })
+      .untilFinalized();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
